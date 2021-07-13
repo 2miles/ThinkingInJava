ec9bf1fd35bd3d69b9d4c1d59f64cdee77569ce9
2miles <miles4599@gmail.com>
2021-07-13 14:43:56 -0700

    changed file naming scheme

--- src/E02_HelloWorld.java
+++ src/E02_HelloWorld.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Ex2_HelloWorld {
+public class E02_HelloWorld {
     //main method
     public static void main(String[] args)
     {
---
+++ src/E03_ATypeName.java
@@ -0,0 +1,14 @@
+
+//: objects/E03_ANameType.java
+/*********************** Exercise 3 ******************************
+ * Turn the code fragments involving ATypeName into a program that
+ * compiles and runs
+ ****************************************************************/
+
+public class E03_ATypeName {
+    public static void main(String[] args) {
+        E03_ATypeName atn = new E03_ATypeName();
+        System.out.println("ATypeName object has been created");
+    }
+}
+
